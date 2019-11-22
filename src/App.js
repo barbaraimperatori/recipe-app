22fb0066bd3540a9925785140d0bfbe9ecc52be0
barbara imperatori <barbarabelen.i@gmail.com>
2019-11-22 15:12:12 -0300

    first commit-initial app

--- src/App.js
+++ src/App.js
@@ -2,25 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const APP_ID = 'a744177d';
+	const APP_KEY = 'a6781d35187976f59d9209f68a31ea8c';
+	const exampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+	return (
+		<div className="App">
+			<h1>Hello cat</h1>
+		</div>
+	);
+};
 
 export default App;
